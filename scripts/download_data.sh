8dee9e7855b12620c8e925663778bcc37d6f02a5
DoDucHuy21020124 <doduchuy1243@gmail.com>
2024-11-20 07:45:22 +0000

    update

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -6,3 +6,8 @@ gdown 1BQvwhSoeDm-caCImtlbcAMzhI8MDsrCZ
 
 unzip -q train_20241023.zip
 unzip -q public\ test.zip
+
+mv public\ test public_test_images
+
+rm train_20241023.zip
+rm public\ test.zip
---
+++ scripts/prepare_data.sh
@@ -0,0 +1,35 @@
+mkdir data/daytime_images
+mkdir data/nighttime_images
+
+python move_file.py --source_folder data/daytime \
+                    --destination_folder data/daytime_labels \
+                    --extension '.txt'
+
+python move_file.py --source_folder data/nighttime \
+                    --destination_folder data/nighttime_labels \
+                    --extension '.txt'
+
+cp -a data/daytime/. data/daytime_images
+cp -a data/nighttime/. data/nighttime_images
+
+python change_labels.py --annotation_folder_path data/nighttime_labels \
+                        --output_folder_path data/nighttime_labels_updated
+
+mkdir data/all_images
+mkdir data/all_labels
+
+python copy_file.py --source_folder data/daytime_images \
+                    --destination_folder data/all_images \
+                    --extension '.jpg'
+
+python copy_file.py --source_folder data/daytime_labels \
+                    --destination_folder data/all_labels \
+                    --extension '.txt'
+
+python copy_file.py --source_folder data/nighttime_images \
+                    --destination_folder data/all_images \
+                    --extension '.jpg'
+
+python copy_file.py --source_folder data/nighttime_labels_updated \
+                    --destination_folder data/all_labels \
+                    --extension '.txt'
